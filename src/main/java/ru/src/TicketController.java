68f50afb1eea8993fa35b4a80d21d482ade7f3e5
Mikhail Dimentman <mdiment@yandex.ru>
2022-10-24 19:53:35 +0300

    some kind of front

---
+++ src/main/java/ru/src/TicketController.java
@@ -0,0 +1,29 @@
+package ru.src;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import javax.annotation.PostConstruct;
+
+@Controller
+public class TicketController {
+
+    @GetMapping("/v2")
+    public String index1(){
+        System.out.println("234234d");
+        return "index_aviatickets";
+    }
+
+    @GetMapping("/registration")
+    public String getRegistration(Model model) {
+        model.addAttribute("user", new User());
+        return "registration";
+    }
+
+    @PostConstruct
+    public String sadas(){
+//        System.out.println(2132);
+        return "123";
+    }
+}
